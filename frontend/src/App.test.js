efb2616a52de18bf14596c4c4abb341e4b74d717
Yoana Ivanova <yoana.ve.ivanova@gmail.com>
2022-07-18 16:42:12 +0300

    Fix React app tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders demo notes app heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const demoNotesHeading = screen.getByText(/Demo Notes App/i);
+  expect(demoNotesHeading).toBeInTheDocument();
 });
